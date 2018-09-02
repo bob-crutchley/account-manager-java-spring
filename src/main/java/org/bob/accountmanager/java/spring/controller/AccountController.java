ebe01cb97fc84826b9b94964bbdc8da837c007a7
bob <bob.crutchley.qa@gmail.com>
2018-09-02 17:54:20 +0100

    made some more changes to the mappings

--- src/main/java/org/bob/accountmanager/java/spring/controller/AccountController.java
+++ src/main/java/org/bob/accountmanager/java/spring/controller/AccountController.java
@@ -2,40 +2,43 @@ package org.bob.accountmanager.java.spring.controller;
 
 import org.bob.accountmanager.java.spring.model.Account;
 import org.bob.accountmanager.java.spring.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController("/api/v1")
+@RestController
+@RequestMapping("/api/v1/account")
 public class AccountController {
 
     private final AccountService accountService;
 
+    @Autowired
     public AccountController(AccountService accountService) {
         this.accountService = accountService;
     }
 
-    @GetMapping
+    @RequestMapping("/all")
     public List<Account> all() {
         return accountService.getAll();
     }
 
-    @GetMapping("{id}")
+    @RequestMapping("/{id}")
     public Account getById(@PathVariable Long id) {
         return accountService.getById(id);
     }
 
-    @PostMapping
+    @RequestMapping("/create")
     public Account create(@RequestBody Account account) {
         return accountService.create(account);
     }
 
-    @PutMapping
+    @RequestMapping("/update")
     public Account update(@RequestBody Account account) {
         return accountService.update(account);
     }
 
-    @DeleteMapping("{id}")
+    @RequestMapping("/delete/{id}")
     public Account deleteById(@PathVariable Long id) {
         return accountService.deleteById(id);
     }
